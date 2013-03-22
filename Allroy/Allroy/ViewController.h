333af74ee40b56aa43861316e76ea3ce1bfe815c
Gary Leija <garyleija@gmail.com>
2013-03-23 00:23:23 +1100

    allroy viewer completed

--- Allroy/Allroy/ViewController.h
+++ Allroy/Allroy/ViewController.h
@@ -1,14 +1,20 @@
 //
 //  ViewController.h
-//  Allroy
+//  Allroy Viewer
 //
 //  Created by Gary Leija on 22/03/13.
 //  Copyright (c) 2013 Gary Leija. All rights reserved.
 //
 
+
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-@property (strong, nonatomic) IBOutlet UIScrollView *imageContainer;
+@interface ViewController : UIViewController <UIScrollViewDelegate>
+@property (strong, nonatomic) IBOutlet UIScrollView *imagesContainer;
 
 @end
+
+
+
+
+
--- Allroy/Allroy/ViewController.m
+++ Allroy/Allroy/ViewController.m
@@ -1,6 +1,6 @@
 //
 //  ViewController.m
-//  Allroy
+//  Allroy Viewer
 //
 //  Created by Gary Leija on 22/03/13.
 //  Copyright (c) 2013 Gary Leija. All rights reserved.
@@ -14,16 +14,66 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad
+- (void)viewDidAppear:(BOOL)animated
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+	[super viewDidAppear:animated];
+    int imageCount = 5;
+    [self loadImagesForPage:0 maximumPages:imageCount*4];
+    self.imagesContainer.contentSize = CGSizeMake(self.imagesContainer.bounds.size.width * 4, self.imagesContainer.bounds.size.height);
 }
+
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
 }
+
+- (void) loadImage:(NSInteger)imageNumber {
+    int totalImages = 5;
+    
+    if([self.imagesContainer viewWithTag:imageNumber] !=nil){
+        return;
+    }
+    
+    NSString* imageName = [NSString stringWithFormat:@"all-%i", imageNumber %totalImages];
+    NSString* imagePath = [[NSBundle mainBundle]pathForResource:imageName ofType:@".jpg"];
+    UIImage* image = [UIImage imageWithContentsOfFile:imagePath];
+    UIImageView* imageView = [[UIImageView alloc]initWithImage:image];
+    CGRect imageViewFrame = self.imagesContainer.bounds;
+    imageViewFrame.origin.x = self.imagesContainer.bounds.size.width * (imageNumber - 1);
+    imageView.frame = imageViewFrame;
+    
+    imageView.contentMode = UIViewContentModeScaleAspectFill;
+    imageView.tag = imageNumber;
+    [self.imagesContainer addSubview:imageView];
+}
+-(void)scrollViewDidEndDecelerating:(UIScrollView*)scrollView{
+    NSInteger page = (int)(scrollView.contentOffset.x / scrollView.bounds.size.width);
+    [self loadImagesForPage:page maximumPages:5*4];
+    // [self loadImagesForPage:page maximumPages:(NSInteger)]
+}
+
+- (void) loadImagesForPage:(NSInteger)page maximumPages:(NSInteger)maximumPages{
+    
+    page += 1;
+    if(page - 1 >=0){
+        [self loadImage:page-1];
+        
+    }
+    [self loadImage:page];
+    if( page + 1 < maximumPages){
+        [self loadImage:page+1];
+    }
+    for (UIView* view in self.imagesContainer.subviews){
+        NSInteger tag = view.tag;
+        if(tag< page - 1 || tag > page + 1)
+            [view removeFromSuperview];
+    }
+    
+}
+
 
 @end
